ab5c8b4e56455b6b9db4f55cbe8cfe49d9ca80e7
YiChenTsai <a0983936190@gmail.com>
2022-01-15 11:59:49 +0800

    讀檔的爬蟲

---
+++ crawler/crawler3.js
@@ -0,0 +1,24 @@
+// 用 async/await 寫法取得證券交易所資料
+const axios = require("axios");
+const { readFile } = require("fs/promises");
+
+(async ()=> {
+  try {
+    let stockNo=await readFile("stock.txt", "utf-8");
+    let queryDate="20220114";
+    
+    let result = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      // 這裡可以放一些設定
+      // params: 放 query string 的參數
+      params:{
+        response: "json",
+        date: queryDate,
+        stockNo,
+      },
+    })
+    console.log(result.data);
+  } catch (err) {
+    // handle err
+    console.log(err);
+  }
+})();
